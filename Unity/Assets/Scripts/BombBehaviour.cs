e00fedc232e9d06b474f2f4baae73851f721b622
Malth <rutkowski.juliete@gmail.com>
2013-10-20 21:02:16 +0200

    BombParty
    Ajout des bombes hors champs, elles apparaissent quand on appuie sur
    espace. Reste à les faire exploser.

---
+++ Unity/Assets/Scripts/BombBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombBehaviour : MonoBehaviour {
+	
+		private bool _bombIsActive = false;
+	public bool BombIsActive 
+	{
+		get {
+			return this._bombIsActive;
+		}
+		set {
+			_bombIsActive = value;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(BombIsActive)			
+		{
+			ActiverBombe();
+		}			
+	}
+	IEnumerator ActiverBombe()
+	{
+		Debug.Log("Bombe Activée");
+		Debug.Log(Time.time);
+		yield return new WaitForSeconds(3);
+		Debug.Log(Time.time);
+		Debug.Log("Bombe Explosée");
+	}
+}
---
+++ Unity/Assets/Scripts/PutBomber.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PutBomber : MonoBehaviour 
+{
+	private Transform _myTransform;
+	public Vector3 test;
+	private bool _bombPresent = false;
+	public bool BombPresent 
+	{
+		get {
+			return this._bombPresent;
+		}
+		set {
+			_bombPresent = value;
+		}
+	}
+
+	private int compteur;
+	public int Compteur 
+	{
+		get {
+			return this.compteur;
+		}
+		set {
+			compteur = value;
+		}
+	}
+	
+	private GameObject _BombeActuelle;
+	public GameObject BombeActuelle
+	{
+		get {
+			return this._BombeActuelle;
+		}
+		set {
+			_BombeActuelle = value;
+		}
+	}	
+	private GameObject[] _stock;
+	public GameObject[] Stock 
+	{
+		get {
+			return this._stock;
+		}
+		set {
+			_stock = value;
+		}
+	}
+	private int _tailleStock;
+	public int TailleStock
+	{
+		get {
+			return this._tailleStock;
+		}
+		set {
+			_tailleStock = value;
+		}
+	}
+
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		_myTransform = this.transform;
+        Stock = GameObject.FindGameObjectsWithTag("BombTest");
+		Compteur = 0;
+		Debug.Log("Stock créé");
+		TailleStock = Stock.Length;
+		Debug.Log(TailleStock);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			if (BombPresent == false)
+			{
+				Debug.Log("Espace appuyé");
+				test = GameObject.FindGameObjectWithTag("Player").transform.localPosition;
+				Debug.Log("Ma position : "+ test);
+				//Poser une bombe
+				BombeActuelle = Stock[compteur];
+				BombeActuelle.BombIsActive(true);
+				//BombeActuelle.activeSelf = true;
+				compteur++;
+			}
+		}
+	}
+}
+ 
