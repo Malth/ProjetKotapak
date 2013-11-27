e1099ba488744e4575e3207d8b74c268b8442390
Malth <rutkowski.juliete@gmail.com>
2013-11-27 18:42:11 +0100

    Commit Juliette  t boutons menu

---
+++ Unity/Assets/Scripts/SpecificActionsScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecificActionsScript : MonoBehaviour 
+{
+	AudioClip son =  null;
+	
+
+void OnMouseUp() { 
+	audio.PlayOneShot(son);
+	Debug.Log ("Texte cliqué : " +this.name);
+	if (this.name == "Quitter")
+	 	Application.Quit();
+	else
+	 	Application.LoadLevel(this.name);
+}
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
+
